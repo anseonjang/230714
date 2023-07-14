3a317304aa47a6dab0c9f52993fe77a4f9bc9479
anseonjang <64743348+anseonjang@users.noreply.github.com>
2023-07-14 18:05:52 +0900

    Add files via upload

---
+++ attrTabLine/js/Attr.js
@@ -0,0 +1,56 @@
+$(function(){
+    $(".btn1").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-01.svg","alt":"배너1"})
+     });
+    
+    $(".btn2").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-02.svg","alt":"배너2"})
+     });
+    
+    $(".btn3").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-03.svg","alt":"배너3"})
+     });
+    
+    $(".btn4").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-04.svg","alt":"배너4"})
+     });
+    
+    $(".btn5").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-05.svg","alt":"배너5"})
+     });
+    
+    $(".btn6").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-06.svg","alt":"배너6"})
+     });
+    
+    $(".btn7").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-07.svg","alt":"배너7"})
+     });
+    
+    $(".btn8").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-08.svg","alt":"배너8"})
+     });
+    
+    $(".btn9").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-09.svg","alt":"배너9"})
+     });
+    
+    $(".btn10").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-10.svg","alt":"배너10"})
+     });
+    
+    $(".btn11").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-11.svg","alt":"배너11"})
+     });
+    
+    $(".btn12").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-12.svg","alt":"배너12"})
+     });
+    
+    $(".btn13").on("click",function(){
+        $(".frame img").attr({"src":"./images/ad-13.svg","alt":"배너13"})
+     });
+
+});
+
+
